6b8cd730488951b5614a0844ac0849d0cae656a1
Agustín <aguformoso@gmail.com>
2015-12-22 14:51:56 -0300

    Search using RIPE libs

--- src/main/java/net/arin/rdap_bootstrap/service/IpV4Bootstrap.java
+++ src/main/java/net/arin/rdap_bootstrap/service/IpV4Bootstrap.java
@@ -16,84 +16,99 @@
  */
 package net.arin.rdap_bootstrap.service;
 
+import java.util.HashMap;
+import java.util.Set;
+
 import net.arin.rdap_bootstrap.service.JsonBootstrapFile.ServiceUrls;
 import net.arin.rdap_bootstrap.service.ResourceFiles.BootFiles;
-
-import java.util.HashMap;
+import net.ripe.ipresource.IpRange;
+import net.ripe.ipresource.UniqueIpResource;
 
 /**
  * @version $Rev$, $Date$
  */
-public class IpV4Bootstrap implements JsonBootstrapFile.Handler
-{
-    private volatile HashMap<Integer,ServiceUrls> allocations = new HashMap<Integer, ServiceUrls>(  );
-    private HashMap<Integer,ServiceUrls> _allocations;
+public class IpV4Bootstrap implements JsonBootstrapFile.Handler {
+	private volatile HashMap<String, ServiceUrls> allocations = new HashMap<String, ServiceUrls>();
+	private HashMap<String, ServiceUrls> _allocations; 
 
-    private ServiceUrls serviceUrls;
-    private String publication;
-    private String description;
+	private ServiceUrls serviceUrls;
+	private String publication;
+	private String description;
 
-    public void loadData( ResourceFiles resourceFiles )
-        throws Exception
-    {
-        JsonBootstrapFile bsFile = new JsonBootstrapFile();
-        bsFile.loadData( resourceFiles.getInputStream( BootFiles.V4.getKey() ), this );
-    }
+	public void loadData(ResourceFiles resourceFiles) throws Exception {
+		JsonBootstrapFile bsFile = new JsonBootstrapFile();
+		bsFile.loadData(resourceFiles.getInputStream(BootFiles.V4.getKey()), this);
+	}
 
-    @Override
-    public void startServices()
-    {
-        _allocations = new HashMap<Integer, ServiceUrls>(  );
-    }
+	@Override
+	public void startServices() {
+		_allocations = new HashMap<String, ServiceUrls>();
+	}
 
-    @Override
-    public void endServices()
-    {
-        allocations = _allocations;
-    }
+	@Override
+	public void endServices() {
+		allocations = _allocations;
+	}
 
-    @Override
-    public void startService()
-    {
-        serviceUrls = new ServiceUrls();
-    }
+	@Override
+	public void startService() {
+		serviceUrls = new ServiceUrls();
+	}
 
-    @Override
-    public void endService()
-    {
-        //nothing to do
-    }
+	@Override
+	public void endService() {
+		// nothing to do
+	}
 
-    @Override
-    public void addServiceEntry( String entry )
-    {
-        int key = Integer.parseInt( entry.split( "/" )[0].split( "\\." )[0] );
-        _allocations.put( key, serviceUrls );
-    }
+	@Override
+	public void addServiceEntry(String entry) {
+		_allocations.put(entry, serviceUrls);
+	}
 
-    @Override
-    public void addServiceUrl( String url )
-    {
-        serviceUrls.addUrl( url );
-    }
+	@Override
+	public void addServiceUrl(String url) {
+		serviceUrls.addUrl(url);
+	}
 
-    public ServiceUrls getServiceUrls( int prefix )
-    {
-        return allocations.get( prefix );
-    }
+	public ServiceUrls getServiceUrls(String prefix) {
+		
+		UniqueIpResource start;
+		try {
+			// /8 single int behaviour
+			new Integer(prefix);
+			start = IpRange.parse(prefix + ".0.0.0/8").getStart();
+		} catch (NumberFormatException e) {
+			start = IpRange.parse(prefix).getStart();
+		}
+		
+		ServiceUrls resultUrl = null;
+		IpRange resultNetwork = IpRange.parse("0.0.0.0/0");
+		final Set<String> keys = allocations.keySet();
+		for (String key : keys) {
+			final IpRange network = IpRange.parse(key);
+			if(network.contains(start) && (resultNetwork.getPrefixLength() < network.getPrefixLength())) {
+				resultNetwork = network;
+				resultUrl = allocations.get(key);
+			}
+		}
+		return resultUrl;
+	}
 
-    @Override
-    public void setPublication( String publication ) { this.publication = publication; }
-    public String getPublication() { return publication; }
+	@Override
+	public void setPublication(String publication) {
+		this.publication = publication;
+	}
 
-    public String getDescription()
-    {
-        return description;
-    }
+	public String getPublication() {
+		return publication;
+	}
 
-    @Override
-    public void setDescription( String description )
-    {
-        this.description = description;
-    }
+	public String getDescription() {
+		return description;
+	}
+
+	@Override
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
