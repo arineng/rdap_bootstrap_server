a3d46d2356beec39a1ef12aa34a14c690e7e79ff
Jasdip Singh <jasdips@arin.net>
2020-09-14 12:38:33 -0400

    Now asserting HTTP response code in the checker script.

--- scripts/rdap_bootstrap_checker.sh
+++ scripts/rdap_bootstrap_checker.sh
@@ -2,38 +2,42 @@
 
 set -e
 
-queries=()
+function query() {
+    echo
+    echo -n "$1 - "
+    status=$(curl -s -o /dev/null -w '%{http_code}' "$1")
+    if [[ $status -ne $2 ]]; then
+        echo "FAIL (expected $2)"
+    else
+        echo "PASS"
+    fi
+    curl -s -I "$1"
+}
 
 # /domain
-queries+=("$1/domain/google.com")
-queries+=("$1//domain/google.foo")
-queries+=("$1/domain/xn--flw351e")
-queries+=("$1/domain/2.in-addr.arpa")
-queries+=("$1/domain/15.in-addr.arpa")
-queries+=("$1/domain/0.0.e.0.1.0.0.2.ip6.arpa")
+query "$1/domain/google.com" 302
+query "$1//domain/google.foo" 302
+query "$1/domain/xn--flw351e" 302
+query "$1/domain/2.in-addr.arpa" 302
+query "$1/domain/15.in-addr.arpa" 302
+query "$1/domain/0.0.e.0.1.0.0.2.ip6.arpa" 302
 
 # /nameserver
-queries+=("$1/nameserver/cnn.com")
-queries+=("$1/nameserver/15.in-addr.arpa")
+query "$1/nameserver/cnn.com" 302
+query "$1/nameserver/15.in-addr.arpa" 302
 
 # /ip
-queries+=("$1/ip/2.0.0.0/8")
-queries+=("$1/ip/15.0.0.0/8")
-queries+=("$1/ip/2c00::/12")
-queries+=("$1/ip/2c00::/13")
-queries+=("$1/ip/3c00::/12")
+query "$1/ip/2.0.0.0/8" 302
+query "$1/ip/15.0.0.0/8" 302
+query "$1/ip/2c00::/12" 302
+query "$1/ip/2c00::/13" 302
+query "$1/ip/3c00::/12" 404
 
 # /autnum
-queries+=("$1/autnum/1")
-queries+=("$1/autnum/272796")
-queries+=("$1/autnum/272797")
+query "$1/autnum/1" 302
+query "$1/autnum/272796" 302
+query "$1/autnum/272797" 404
 
 # /entity
-queries+=("$1/entity/ODIN19-ARIN")
-queries+=("$1/entity/IRT-APNIC-AP")
-
-for query in "${queries[@]}"; do
-    echo
-    echo "$query"
-    curl -s -I "${query}"
-done
+query "$1/entity/ODIN19-ARIN" 302
+query "$1/entity/IRT-APNIC-AP" 302
